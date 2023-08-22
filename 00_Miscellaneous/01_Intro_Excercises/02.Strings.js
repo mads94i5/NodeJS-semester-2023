a01d6434b3e5264a715a8e984645a98e2758ed1e
mads94i5 <mads94i5@stud.kea.dk>
2023-08-22 11:47:16 +0200

    All exercises done

---
+++ 00_Miscellaneous/01_Intro_Excercises/02.Strings.js
@@ -0,0 +1,46 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+const result1 = +numberOne + +numberTwo;
+console.log(result1);
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const result2 = parseFloat(+anotherNumberOne + +anotherNumberTwo).toFixed(2);
+console.log(result2);
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+const average = parseFloat((one+two+three)/3).toFixed(5);
+console.log(average);
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+
+// Get me the character "c"
+
+const lastLetter = letters.charAt(letters.length-1);
+console.log(lastLetter);
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+const capitalized = fact.substring(0, 17) + fact.charAt(17).toUpperCase() + fact.substring(18);
+console.log(capitalized);
+// --------------------------------------
---
+++ 00_Miscellaneous/01_Intro_Excercises/03.Objects.js
@@ -0,0 +1,31 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObj = { message: "Hello, earthling! I bring peace." };
+
+// Log the message 
+console.log(myObj.message);
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+const objOfMe = { name: "Mads", age: 33 };
+console.log(objOfMe.name, objOfMe.age);
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = { isAllowed: true };
+
+// make a rule called isAllowed and let the value be true
+console.log(stackOverflow.isAllowed);
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = { about: "Just a tribute." };
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+console.log(thisSong.about);
+// --------------------------------------
