b063c4ebaea60ad416f617489e009c201c7043eb
mads94i5 <mads94i5@stud.kea.dk>
2023-08-22 10:46:08 +0200

    Basics

--- 00_Miscellaneous/01_Intro_Excercises/1.Basics.js
+++ 00_Miscellaneous/01_Intro_Excercises/1.Basics.js
@@ -18,5 +18,14 @@ const number = 1;
 // Add the year plus the number
 // The result should be 2023
 // You cannot touch line 1 or 2
-console.log(+year + number);
+const result1 = +year + number;
+const result2 = Number(year) + number;
+const result3 = parseInt(year) + number;
+console.log(result1, result2, result3);
 // --------------------------------------
+
+// Will return NaN
+console.log(Number("23a"));
+console.log(+"23a");
+// Will return 23:
+console.log(parseInt("23a"));
